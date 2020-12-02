a5ff72ecb99b478ec2e06efb2e41ccc96896bdec
zuka <kid00345@hotmail.com>
2020-12-02 15:07:51 +0700

    update_bykit_01

--- resources/views/doctor_search.blade.php
+++ resources/views/doctor_search.blade.php
@@ -8,14 +8,24 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/doctor_search.css') }}">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-    <!-- Bootstrap CSS -->
-    {{--
-    <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet"
-        type="text/css"> --}}
+    {{-- Header For search Doctor CSS --}}
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/doctor_search.css') }}">
+
+    {{-- Footer --}}
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/footer.css') }}">
+
+    {{-- Swiper --}}
+    <link rel="stylesheet" href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}">
+
+    {{-- Icon Material --}}
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('vendor/materialize/css/materialize.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('vendor/Skeleton/src/avnSkeleton/avnSkeleton.css') }}">
+
+    {{-- <link rel="stylesheet" href="{{ asset('vendor/jquery-ui/jquery-ui.css') }}"> --}}
 
 </head>
 
@@ -34,13 +44,30 @@
             </div>
         </div>
 
-        @section('Show_data_search')
+        <div id="wait_load">
+            @section('Show_data_search')
+            @show
+        </div>
+
+
+
+        @include('footer.footer')
+        @section('footer')
         @show
     </div>
 
     <script src="{{ asset('js/jquery-3.5.1.min.js') }}"> </script>
     <script src="{{ asset('vendor/materialize/js/materialize.min.js') }}"></script>
-    <script src="{{ asset('js/search_doctor/form_search.js') }}"> </script>
+    <script src="{{ asset('js/doctor_search/doctor_search.js') }}"> </script>
+    <script src="{{ asset('js/header/header.js') }}"> </script>
+    <script src="{{ asset('vendor/Skeleton/src/avnPlugin.js') }}"></script>
+    <script src="{{ asset('vendor/Skeleton/src/avnSkeleton/avnSkeleton.js') }}"></script>
+
+    <!-- Swiper JS -->
+    <script src="{{ asset('vendor/swiper/swiper-bundle.min.js') }}"></script>
+
+    {{-- <script src="{{ asset('vendor/jquery-ui/jquery-ui.js') }}"></script> --}}
+
 </body>
 
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,24 +1,32 @@
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>ข้อมูลแพทย์</title>
-        {{-- icon title --}}
-    <link rel="icon" href="{{asset('')}}assets/logo/iconsamitivejc400x400.jpg" type="image/gif" sizes="16x16">
-        <!-- font-awesome -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-        <!-- Compiled and minified CSS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    </head>
-    @include('header.header');
-    <body>
-qqqqqqqqqqqqqqqqqqq
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ข้อมูลแพทย์</title>
+    {{-- icon title --}}
+    <link rel="icon" href="{{ asset('') }}assets/logo/iconsamitivejc400x400.jpg" type="image/gif" sizes="16x16">
+    <!-- font-awesome -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    {{-- Header CSS --}}
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/doctor_search.css') }}">
+</head>
+@include('header.header')
+@section('header')
+@show
+
+<body>
+
+</body>
+
 </html>
- <!-- Compiled and minified JavaScript -->
-<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+<!-- Compiled and minified JavaScript -->
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+    crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
